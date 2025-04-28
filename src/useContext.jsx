5deaaed697206d5f19f0d6fd5752e57bb513598f
Shivam <101854133+MrShivam98@users.noreply.github.com>
2025-04-28 10:36:51 +0530

    Using Sample Data as a Fallback

--- src/useContext.jsx
+++ src/useContext.jsx
@@ -1,12 +1,12 @@
 import { createContext, useState, useEffect } from 'react';
 import sampleData from "../sampleData.json"
 
-export const MyContext = createContext(sampleData);
+export const MyContext = createContext([]);
 
 export function MyContextProvider({ children }) {
     const [sharedData, setSharedData] = useState(() => {
         const stored = localStorage.getItem("dealsData");
-        return stored ? JSON.parse(stored) : [];
+        return stored ? JSON.parse(stored) : sampleData;
     });
 
     useEffect(() => {
